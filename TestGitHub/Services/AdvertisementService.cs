0e605b2950a00f5961b3eecf0622b69a7372a933
Tommy Hinrichs <tommy.hinrichs@gmail.com>
2011-01-31 22:03:24 -0800

    Stub WCF service setup for Adverts

---
+++ TestGitHub/Services/AdvertisementService.cs
@@ -0,0 +1,36 @@
+﻿using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace Services
+{
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class AdvertisementService : IAdvertisementService
+    {
+        #region IAdvertisementService Members
+
+        [WebInvoke(Method = "POST")]
+        public Advertisement Create(Advertisement person)
+        {
+            return null;
+        }
+
+        [WebGet(UriTemplate = "{addId}")]
+        public Advertisement Read(int addId)
+        {
+            return null;
+        }
+
+        [WebInvoke(UriTemplate = "{addId}", Method = "PUT")]
+        public Advertisement Update(int addId, Advertisement person)
+        {
+            return null;
+        }
+
+        [WebInvoke(UriTemplate = "{addId}", Method = "DELETE")]
+        public void Delete(string id)
+        {
+        }
+
+        #endregion
+    }
+}
