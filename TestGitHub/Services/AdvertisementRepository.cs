f105a751c622e44250cc4e19a163dd7f5309a844
Tommy Hinrichs <tommy.hinrichs@gmail.com>
2011-01-31 22:19:12 -0800

    Cleanup code... get stub working... generic crud service by type?

---
+++ TestGitHub/Services/AdvertisementRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Services
+{
+    public static class AdvertisementRepository
+    {
+        public static void Update(Advertisement advertisement)
+        {
+         
+        }
+
+        public static Advertisement GetAdvertById(string addId)
+        {
+            return new Advertisement();
+        }
+    }
+}
