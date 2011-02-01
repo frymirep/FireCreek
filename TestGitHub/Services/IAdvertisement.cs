3d1a44c13046748049b6f64b098a4f121dde5680
Tommy Hinrichs <tommy.hinrichs@gmail.com>
2011-01-31 20:25:51 -0800

    Setup WCF fundamentals

---
+++ TestGitHub/Services/IAdvertisement.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Services
+{
+    [DataContract(Namespace = "")]
+    public class Advertisement
+    {
+        [DataMember]
+        int Identifier { get; set; }
+        [DataMember]
+        byte[] AdvertContent { get; set; }
+    }
+}
---
+++ TestGitHub/Services/IAdvertisementService.cs
@@ -0,0 +1,22 @@
+﻿// inspiration: http://geekswithblogs.net/michelotti/archive/2010/08/21/restful-wcf-services-with-no-svc-file-and-no-config.aspx
+
+using System.ServiceModel;
+
+namespace Services
+{
+    [ServiceContract]
+    public interface IAdvertisementService
+    {
+        [OperationContract]
+        Advertisement GetAdvert(int addId);
+
+        [OperationContract]
+        Advertisement InsertPerson(Advertisement person);
+
+        [OperationContract]
+        Advertisement UpdatePerson(string id, Advertisement person);
+
+        [OperationContract]
+        void DeletePerson(string id);
+    }
+}
