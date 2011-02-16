471b1ef8f945f961b058e90b14c7c9563adee899
Tommy Hinrichs <tommy.hinrichs@gmail.com>
2011-02-15 22:20:54 -0800

    fix bug

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,5 +1,6 @@
 ﻿Ext.setup({
     tabletStartupScreen: 'tablet_startup.png',
+    phoneStartupScreen: 'phone_startup.png',
     icon: 'icon.png',
     glossOnIcon: false,
         onReady: function () {        
